8e786e0ae3659092b88018da03b3fb14c077fa75
Sebastian R <josesrb10@outlook.com>
2021-01-26 12:11:49 -0600

    Client Skeleton

---
+++ client/src/app/app.component.ts
@@ -0,0 +1,32 @@
+import { HttpClient } from '@angular/common/http';
+import { error } from '@angular/compiler/src/util';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+
+  title = 'The Dating App';
+  users: any;
+
+  constructor(
+    private _httpClient: HttpClient
+  ) { }
+
+  ngOnInit() {
+    this.GetUsers();
+  }
+
+  GetUsers() {
+    this._httpClient.get('https://localhost:5001/api/users')
+      .subscribe(response => {
+        this.users = response;
+      }, error => {
+        console.log(error);
+      });
+  }
+
+}
